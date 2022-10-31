d5cf2ac92ddc8db7046dc54a8ceca6dfeb220634
kodakodu <kodakodu@gmail.com>
2022-10-31 15:29:30 +0100

    mock-pokemon.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+let pokemons = require('./mock-pokemon.js');
 const app = express();
 
 const port = 3000;
@@ -9,9 +9,10 @@ app.get('/', (req, res) => {
 });
 app.get('/api/pokemons/:id', (req, res) => {
   const id = req.params.id;
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
   res.send(`Vous aves demandé le pokemon n°${id}`);
 });
-console.log('Hello !');
+//console.log('Hello !');
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
