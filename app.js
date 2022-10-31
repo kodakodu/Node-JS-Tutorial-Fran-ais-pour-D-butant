73603b959f97885d46ff9f4e0d6b000f0084664b
kodakodu <kodakodu@gmail.com>
2022-10-31 11:47:07 +0100

    params

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
-
+app.get('/api/pokemons/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Vous aves demandé le pokemon n°${id}`);
+});
 console.log('Hello !');
 
 app.listen(port, () => {
