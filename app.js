a0868d61c2ad07227cac34c4c5dc49af78b63af2
kodakodu <kodakodu@gmail.com>
2022-11-02 15:26:52 +0100

    2:02

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const { success, notsuccess } = require('./helper');
 let pokemons = require('./mock-pokemon.js');
 const app = express();
 
 const port = 3000;
 
-
-
-app.use((req, res, next) => {
-  console.log(`URL: ${req.url}`);
-  next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
