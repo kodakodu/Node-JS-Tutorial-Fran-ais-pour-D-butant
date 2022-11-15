a905626ae26bb6d93a941f9ea34444f0e37345af
kodakodu <kodakodu@gmail.com>
2022-11-15 09:59:05 +0100

    4:40

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ require('./src/routes/createPokemon')(app);
 require('./src/routes/updatePokemon')(app);
 require('./src/routes/deletePokemon')(app);
 
+app.use(({ res }) => {
+  const message =
+    'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.';
+  res.status(404).json(message);
+});
+
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
