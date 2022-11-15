910732cf77e62f125e0e8325b5a2d8c9c73ae682
kodakodu <kodakodu@gmail.com>
2022-11-15 11:38:59 +0100

    5:13

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,3 +1,4 @@
+const { ValidationError } = require('sequelize');
 const { Pokemon } = require('../db/sequelize');
 
 module.exports = (app) => {
@@ -5,19 +6,23 @@ module.exports = (app) => {
     const id = req.params.id;
     Pokemon.update(req.body, {
       where: { id: id },
-    }).then((_) => {
-      return Pokemon.findByPk(id).then((pokemon) => {
-        if (pokemon === null) {
-          const message = `Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant.`;
-          return res.status(404).json({ message });
-        }
-        const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
-        res.json({ message, data: pokemon });
-      });        
     })
-    .catch((error) => {
-      const message = `Le pokemon n'a pas pu être modifié. Réessayez dans quelques instants.`;
-      res.status(500).json({ message, data: error });
-    });
+      .then((_) => {
+        return Pokemon.findByPk(id).then((pokemon) => {
+          if (pokemon === null) {
+            const message = `Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant.`;
+            return res.status(404).json({ message });
+          }
+          const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
+          res.json({ message, data: pokemon });
+        });
+      })
+      .catch((error) => {
+        if (error instanceof ValidationError) {
+          return res.status(400).json({ message: error.message, data: error });
+        }
+        const message = `Le pokemon n'a pas pu être modifié. Réessayez dans quelques instants.`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
