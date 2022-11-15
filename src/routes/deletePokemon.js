017c2598230a2e8f0934ced8e93029954a05c026
kodakodu <kodakodu@gmail.com>
2022-11-15 11:05:36 +0100

    4:54

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -3,13 +3,21 @@ const { Pokemon } = require('../db/sequelize');
 module.exports = (app) => {
   app.delete('/api/pokemons/:id', (req, res) => {
     Pokemon.findByPk(req.params.id).then((pokemon) => {
+      if (pokemon === null) {
+        const message = `Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant.`;
+        res.status(404).json({ message });
+      }
       const pokemonDeleted = pokemon;
-      Pokemon.destroy({
+      return Pokemon.destroy({
         where: { id: pokemon.id },
       }).then((_) => {
         const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`;
         res.json({ message, data: pokemonDeleted });
       });
+    })
+    .catch((error) => {
+      const message = `Le pokemon n'a pas pu être supprimé. Réessayez dans quelques instants.`;
+      res.status(500).json({ message, data: error });
     });
   });
 };
