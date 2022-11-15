92ecd3fae6481bda2f556414f91fcdd16c337c7f
kodakodu <kodakodu@gmail.com>
2022-11-15 10:42:39 +0100

    4:46

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -2,9 +2,18 @@ const { Pokemon } = require('../db/sequelize');
 
 module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
-    Pokemon.findByPk(req.params.id).then((pokemon) => {
-      const message = 'Un pokémon a bien été trouvé.';
-      res.json({ message, data: pokemon });
-    });
+    Pokemon.findByPk(req.params.id)
+      .then((pokemon) => {
+        if (pokemon === null) {
+          const message = `Le pokemon ndemandé n'existe pas. Réessayez avec un autre identifiant.`;
+          return res.status(404).json({ message });
+        }
+        const message = 'Un pokémon a bien été trouvé.';
+        res.json({ message, data: pokemon });
+      })
+      .catch((error) => {
+        const message = `Le pokemon n'a pas été recupéré. Réessayez dans un instant.`;
+        res.status(500).json({ message, data: error });
+      });
   });
 };
