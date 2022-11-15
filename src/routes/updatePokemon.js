57e6c888a3bde22d187cca125ad4cf344b55bf4f
kodakodu <kodakodu@gmail.com>
2022-11-15 11:00:36 +0100

    4:52

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -6,10 +6,18 @@ module.exports = (app) => {
     Pokemon.update(req.body, {
       where: { id: id },
     }).then((_) => {
-      Pokemon.findByPk(id).then((pokemon) => {
+      return Pokemon.findByPk(id).then((pokemon) => {
+        if (pokemon === null) {
+          const message = `Le pokemon demandé n'existe pas. Réessayez avec un autre identifiant.`;
+          return res.status(404).json({ message });
+        }
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
         res.json({ message, data: pokemon });
-      });
+      });        
+    })
+    .catch((error) => {
+      const message = `Le pokemon n'a pas pu être modifié. Réessayez dans quelques instants.`;
+      res.status(500).json({ message, data: error });
     });
   });
 };
