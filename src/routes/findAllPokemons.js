5c00f234325f80559f553ebfbd2645b51ac94bbe
kodakodu <kodakodu@gmail.com>
2022-11-18 12:46:22 +0100

    5:50

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -5,21 +5,21 @@ module.exports = (app) => {
     if (req.query.name) {
       const name = req.query.name;
       return Pokemon.findAll({ where: { name: name } }).then((pokemons) => {
-        const message = `Il y a ${pokemons.length} pokemons qui correspondent au terle de recherche ${name}.`;
+        const message = `Il y a ${pokemons.length} pokemons qui correspondent au terme de recherche ${name}.`;
         res.json({ message, data: pokemons });
       });
+    } else {
+      try {
+        Pokemon.findAll()
+          .then((pokemons) => {
+            const message = 'La liste des pokémons a bien été récupérée.';
+            res.json({ message, data: pokemons });
+          })
+          .catch((error) => {
+            const message = `La liste des pokemons n'a pas pu être récupérée. Réessayer dans quelques instants.`;
+            res.status(500).json({ message, data: error });
+          });
+      } catch (error) {}
     }
-try {
-  Pokemon.findAll()
-    .then((pokemons) => {
-      const message = 'La liste des pokémons a bien été récupérée.';
-      res.json({ message, data: pokemons });
-    })
-    .catch((error) => {
-      const message = `La liste des pokemons n'a pas pu être récupérée. Réessayer dans quelques instants.`;
-      res.status(500).json({ message, data: error });
-    });
-} catch (error) {}
-
   });
 };
