b6bd5baafc44b4545aee751a0c41a649fa6968ce
kodakodu <kodakodu@gmail.com>
2022-11-28 11:30:01 +0100

    6:57

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,7 @@
 const { User } = require('../db/sequelize');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const privateKey = require('../auth/private_key');
 
 module.exports = (app) => {
   app.post('/api/login', (req, res) => {
@@ -16,8 +18,15 @@ module.exports = (app) => {
               const message = `Le mot de passe est incorrect.`;
               return res.status(401).json({ message });
             }
+
+            //JWT
+
+            const token = jwt.sign({ userId: user.id }, privateKey, {
+              expiresIn: '24h',
+            });
+
             const message = `L'utilisateur a été connecté avec succès.`;
-            return res.json({ message, data: user });
+            return res.json({ message, data: user, token });
           });
       })
       .catch((error) => {
