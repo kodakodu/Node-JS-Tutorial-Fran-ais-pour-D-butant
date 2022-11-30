e9c928cfa525b2e37f7cfc6cc06f73df3b231a85
kodakodu <kodakodu@gmail.com>
2022-11-30 17:01:02 +0100

    7:32

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var path = require('path');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
   flags: 'a',
